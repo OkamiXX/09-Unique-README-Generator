c4e2d09e8c802d4baa479b0ce110a878c0a407f0
Carlos E. Osuna Izquierdo <cosunai@hotmail.com>
2023-06-20 22:49:26 -0600

    Finished the program

---
+++ dist/README.md
@@ -0,0 +1,62 @@
+# Readme Generator TEST
+
+## Github URL 🌎:
+
+['OkamiXX](https://github.com/'OkamiXX/)
+
+## Description 📝:
+
+    My project is about generating a README file based on the user input.
+
+
+## Table of Contents 🗂:
+
+* [Installations](#dependencies)
+
+* [Usage](#usage)
+
+
+* [License](#license)
+
+
+* [Contributors](#contributors)
+
+* [Test](#test)
+
+
+## 'Dependencies' Needed 🧿 :
+
+To install dependencies, run these commands:
+
+```
+npm i inquirer
+```
+
+
+## Usage 
+
+Generate a README FILE
+
+## License 📢©️
+
+    Copyright © MIT. All rights reserved. 
+      
+    Licensed under the MIT license.
+
+![GitHub license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+
+## Contributors 🥳:
+
+    Me, Myself and I
+
+Email Information:  okamix@gmail.com
+
+
+## Tests 🧪:
+
+To run tests, run these commands:
+
+```
+npm test jest
+```
+  
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,98 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+  return '';
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    );
+  }
+  return '';
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License 📢©️
+
+    Copyright © ${license}. All rights reserved. 
+      
+    Licensed under the ${license} license.`
+
+    );
+  };
+  return ''
+};
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Github URL 🌎:
+
+[${data.github}](https://github.com/${data.github}/)
+
+## Description 📝:
+
+    ${data.description}
+
+
+## Table of Contents 🗂:
+
+* [Installations](#dependencies)
+
+* [Usage](#usage)
+
+${renderLicenseLink(data.license)}
+
+* [Contributors](#contributors)
+
+* [Test](#test)
+
+
+## 'Dependencies' Needed 🧿 :
+
+To install dependencies, run these commands:
+
+\`\`\`
+${data.dependencies}
+\`\`\`
+
+
+## Usage 
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+${renderLicenseBadge(data.license)}
+
+## Contributors 🥳:
+
+    ${data.contributors}
+
+Email Information:  ${data.email}
+
+
+## Tests 🧪:
+
+To run tests, run these commands:
+
+\`\`\`
+${data.test}
+\`\`\`
+  
+`;
+}
+
+module.exports = generateMarkdown;
